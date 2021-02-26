3a77e65d5a02de7b8284a3532b6190ce6858f8aa
VARZero <cydiazero@gmail.com>
2021-02-26 20:40:39 +0900

    디코더 1차

--- useVerilog/makemodcpu16.v
+++ useVerilog/makemodcpu16.v
@@ -10,10 +10,25 @@ module vz16 (
     output [15:0] addrBus
 )
     reg [15:0] pc;
+    reg [15:0] status;
     reg [15:0] registers[13]; // sp ~ r11
+    reg [2:0] ALUo;
+    reg option;
+    reg ALU, LS, STACK, Shift;
     always @(posedge clk) begin
         // decoder
-        ALU = instrBus[3];
+        ALU <= instrBus[3];
+        ALUo <= (ALU) ? instrBus[2:0] : 0;
+        
+        option <= instrBus[0];
+        LS <= (ALU) ? 0 : ~instrBus[2] & instrBus[1];
+        STACK <= (ALU) ? 0 : instrBus[2] & ~instrBus[1];
+        Shift <= (ALU) ? 0 : instrBus[2] & instrBus[1];
+        
+        Rn <= instrBus[7:4];
+        R1 <= instrBus[8:11];
+        R2 <= instrBus[15:12];
+
         
     end
 endmodule
